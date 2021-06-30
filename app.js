a1e82c179366c49ebca2d8681e6c50c0242209bc
AQSA <aqsa1704a@gmal.com>
2021-06-30 12:07:49 +0500

    ..

--- app.js
+++ app.js
@@ -12,26 +12,6 @@ app.use(express.json());
 app.use("/api/v1/transactions", transactions);
 app.use("/api/v1/customers", customers);
 
-app.patch("/update", (request, response) => {
-  const {
-    sender_Account_No,
-    receiver_Account_No,
-    receiver_newBalance,
-    sender_newBalance,
-  } = request.body;
-  const db = dbserver.getDbServiceInstance();
-  const result = db.updateBalanceById(
-    sender_Account_No,
-    receiver_Account_No,
-    receiver_newBalance,
-    sender_newBalance
-  );
-
-  result
-    .then((data) => response.json({ success: data }))
-    .catch((err) => console.log(err));
-});
-
 const PORT = process.env.PORT || 5000;
 if (process.env.NODE_ENV == "production") {
   app.use(express.static("client/build"));
